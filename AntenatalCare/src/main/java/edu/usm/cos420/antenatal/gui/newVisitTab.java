afbe34ea0c52a3934ad256b727021d0484a63ce9
Ross McLain <ross.mclain@maine.edu>
2016-02-17 23:51:05 -0500

    Added the first Text Entry box. It will be easy to add more

--- AntenatalCare/src/main/java/edu/usm/cos420/antenatal/gui/newVisitTab.java
+++ AntenatalCare/src/main/java/edu/usm/cos420/antenatal/gui/newVisitTab.java
@@ -1,6 +1,9 @@
 package edu.usm.cos420.antenatal.gui;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -9,15 +12,34 @@ public class newVisitTab extends JPanel {
 	JPanel firstPanel;
 	JLabel firstLabel;
 	public newVisitTab(){
-		tabbedPane = new JTabbedPane();
 		firstPanel = new JPanel();
-		firstLabel = new JLabel("NEW VISIT");
-		
-		tabbedPane.addTab("New Visit", firstPanel);	
 		
 		
+		firstPanel.setLayout(new FlowLayout());
+		
+		tabbedPane = new JTabbedPane();
+		
+		firstLabel = new JLabel("Name:");
 		firstPanel.add(firstLabel);
+		
+		JTextField firstText = new JTextField(10);
+		firstPanel.add(firstText);
+		
+		
+		JButton submit = new JButton("Submit");
+		firstPanel.add(submit);
+		submit.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(1);
+				
+			}
+			
+		});
+		
 		setLayout(new BorderLayout());
+		tabbedPane.addTab("New Visit", firstPanel);	
 		tabbedPane.setTabPlacement(JTabbedPane.TOP);
 		tabbedPane.setTabLayoutPolicy(JTabbedPane.WRAP_TAB_LAYOUT);
 		
