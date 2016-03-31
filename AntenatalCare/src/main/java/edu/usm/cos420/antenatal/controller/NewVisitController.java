e1b4cc26d9ec9b01726495368f232140f9b79009
Naomi Heuer <heuer.naomi@gmailc.om>
2016-03-31 14:36:17 -0400

    added some verification of fields -still need to: --fix the updating of fields --redding out boxes

--- AntenatalCare/src/main/java/edu/usm/cos420/antenatal/controller/NewVisitController.java
+++ AntenatalCare/src/main/java/edu/usm/cos420/antenatal/controller/NewVisitController.java
@@ -13,49 +13,103 @@ import java.time.LocalDate;
  * Created by aaron on 3/11/2016.
  */
 public class NewVisitController implements ActionListener {
-  private final AntenatalController controller;
-  private final newVisitTab panel;
-  private String id = null;
+	private final AntenatalController controller;
+	private final newVisitTab panel;
+	private String id = null;
 
-  public NewVisitController(AntenatalController controller) {
-    this.controller = controller;
-    this.panel = new newVisitTab(this);
-  }
+	public NewVisitController(AntenatalController controller) {
+		this.controller = controller;
+		this.panel = new newVisitTab(this);
+	}
 
-  public NewVisitController(AntenatalController controller, AntenatalVisit visitInfo) {
-    this(controller);
-    this.id = visitInfo.getID();
+	public NewVisitController(AntenatalController controller, AntenatalVisit visitInfo) {
+		this(controller);
+		this.id = visitInfo.getID();
 
-    // Refactor this?
-    this.panel.setFormData(visitInfo);
+		// Refactor this?
+		this.panel.setFormData(visitInfo);
 
-  }
+	}
 
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    switch (e.getActionCommand()) {
-      case "Submit": {
-        AntenatalVisit visit = getVisitObject();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+		case "Submit": {
 
-        if (id == null){
-          String nextId = controller.getNextId();
-          visit.setId(nextId);
-          controller.submitNewVisit(visit);
-        }
-        else{
-          visit.setId(id);
-          controller.updateVisit(visit);
-        }
-      }
-    }
-  }
+			VisitForm form = this.panel.getForm();
+			//verify fields
+			if(verifyFields(form) == true){
+				AntenatalVisit visit = getVisitObject();
+				if (id == null){
+					String nextId = controller.getNextId();
+					visit.setId(nextId);
+					controller.submitNewVisit(visit);
+				}
+				else{
+					visit.setId(id);
+					controller.updateVisit(visit);
+				}
+			}else{//if invalid
+				System.out.println("Required fields are not valid.");
+			}
 
-  public JPanel getPanel() {
-    return panel.getPanel();
-  }
+			System.exit(0);
+		}
+		}
+	}
 
-  public AntenatalVisit getVisitObject() {
-    VisitForm form = panel.getForm();
-    return new AntenatalVisit(form);
-  }
+	public static boolean verifyFields(VisitForm form){
+		//holds the window view word contents
+		String errorMessage = "Invalid Data Entries: \n";
+		Boolean errorsExist = false;
+
+		//test blood pressure sys/dia
+		//sys
+		if(form.getSystolicBP() == -1){
+			errorsExist = true;
+			errorMessage += "-Invalid Systolic Blood Pressure\n";
+		}
+		//dia
+		if(form.getDiastolicBP() == -1){
+			errorsExist = true;
+			errorMessage += "-Invalid Diastolic Blood Pressure\n";
+		}
+
+		//test weight
+		if(form.getPatientWeight() < 0){
+			errorsExist = true;
+			errorMessage += "-Invalid Patient Height\n";
+		}
+
+		//test height
+		if(form.getPatientHeight() < 0){
+			errorsExist = true;
+			errorMessage += "-Invalid Patient Weight\n";
+		}
+
+		//test more specific things
+
+		//if pos sickle cell ensure type is selected
+		if(form.getSicklingStatus().equals("Positive") && form.getSicklingStatus().equals("")){
+			errorsExist = true;
+			errorMessage += "-Please Add Sickling Type, or select N/A";
+		}
+
+		//TODO: Add more error messages
+
+		//if error found produce pop up
+		if(errorsExist == true){
+			JOptionPane.showMessageDialog(null, errorMessage);
+		}
+		return false;
+	}
+
+	public JPanel getPanel() {
+		return panel.getPanel();
+	}
+
+	public AntenatalVisit getVisitObject() {
+		VisitForm form = panel.getForm();
+		return new AntenatalVisit(form);
+	}
 }
