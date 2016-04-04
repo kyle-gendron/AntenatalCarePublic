91f5903c6ed473a09c49bc5c0f3062c1a76a040e
Naomi Heuer <heuer.naomi@gmailc.om>
2016-04-04 00:55:03 -0400

    added modified verification of fields that are required, currently just a pop up
    disabled some fields that should be enabled after other fields

--- AntenatalCare/src/main/java/edu/usm/cos420/antenatal/controller/NewVisitController.java
+++ AntenatalCare/src/main/java/edu/usm/cos420/antenatal/controller/NewVisitController.java
@@ -3,6 +3,7 @@ package edu.usm.cos420.antenatal.controller;
 import edu.usm.cos420.antenatal.domain.AntenatalVisit;
 import edu.usm.cos420.antenatal.gui.VisitForm;
 import edu.usm.cos420.antenatal.gui.newVisitTab;
+import edu.usm.cos420.antenatal.view.impl.AntenatalView;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -30,13 +31,13 @@ public class NewVisitController implements ActionListener {
 		this.panel.setFormData(visitInfo);
 
 	}
-
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		switch (e.getActionCommand()) {
 		case "Submit": {
 
-			VisitForm form = this.panel.getForm();
+			VisitForm form = panel.getForm();
 			//verify fields
 			if(verifyFields(form) == true){
 				AntenatalVisit visit = getVisitObject();
@@ -44,6 +45,7 @@ public class NewVisitController implements ActionListener {
 					String nextId = controller.getNextId();
 					visit.setId(nextId);
 					controller.submitNewVisit(visit);
+					AntenatalView.removeCurrentTab();
 				}
 				else{
 					visit.setId(id);
@@ -53,7 +55,6 @@ public class NewVisitController implements ActionListener {
 				System.out.println("Required fields are not valid.");
 			}
 
-			System.exit(0);
 		}
 		}
 	}
@@ -65,24 +66,24 @@ public class NewVisitController implements ActionListener {
 
 		//test blood pressure sys/dia
 		//sys
-		if(form.getSystolicBP() == -1){
+		if(form.getSystolicBP() <= 0){
 			errorsExist = true;
 			errorMessage += "-Invalid Systolic Blood Pressure\n";
 		}
 		//dia
-		if(form.getDiastolicBP() == -1){
+		if(form.getDiastolicBP() <= 0){
 			errorsExist = true;
 			errorMessage += "-Invalid Diastolic Blood Pressure\n";
 		}
 
 		//test weight
-		if(form.getPatientWeight() < 0){
+		if(form.getPatientWeight() <= 0){
 			errorsExist = true;
 			errorMessage += "-Invalid Patient Height\n";
 		}
 
 		//test height
-		if(form.getPatientHeight() < 0){
+		if(form.getPatientHeight() <= 0){
 			errorsExist = true;
 			errorMessage += "-Invalid Patient Weight\n";
 		}
@@ -90,7 +91,7 @@ public class NewVisitController implements ActionListener {
 		//test more specific things
 
 		//if pos sickle cell ensure type is selected
-		if(form.getSicklingStatus().equals("Positive") && form.getSicklingStatus().equals("")){
+		if(form.getSicklingStatus().equals("Positive") && form.getSicklingType().equals("")){
 			errorsExist = true;
 			errorMessage += "-Please Add Sickling Type, or select N/A";
 		}
@@ -100,8 +101,9 @@ public class NewVisitController implements ActionListener {
 		//if error found produce pop up
 		if(errorsExist == true){
 			JOptionPane.showMessageDialog(null, errorMessage);
+			return false;
 		}
-		return false;
+		return true;
 	}
 
 	public JPanel getPanel() {
