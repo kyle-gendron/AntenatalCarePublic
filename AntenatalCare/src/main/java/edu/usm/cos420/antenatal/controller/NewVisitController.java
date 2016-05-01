b55ea611235a7d66d1d2168110c70b41564141ca
Ross McLain <ross.mclain@maine.edu>
2016-05-01 19:22:54 -0400

    took out verifying fields in controller

--- AntenatalCare/src/main/java/edu/usm/cos420/antenatal/controller/NewVisitController.java
+++ AntenatalCare/src/main/java/edu/usm/cos420/antenatal/controller/NewVisitController.java
@@ -38,7 +38,6 @@ public class NewVisitController implements ActionListener {
 
         VisitForm form = panel.getForm();
         //verify fields
-        if (verifyFields(form)) {
           PregnancyVisit visit = getVisitObject();
           if (id == null) {
             String nextId = controller.getNextId();
@@ -49,59 +48,8 @@ public class NewVisitController implements ActionListener {
             visit.setId(id);
             controller.updateVisit(visit);
           }
-        } else {//if invalid
-          System.out.println("Required fields are not valid.");
-        }
-
       }
     }
-  }
-
-  public static boolean verifyFields(VisitForm form) {
-    //holds the window view word contents
-    StringBuilder errorMessage = new StringBuilder("<html><div style='text-align=center;'>Invalid Data Entries: <br>\n");
-    Boolean errorsExist = false;
-
-    //test blood pressure sys/dia
-    //sys
-    if (form.getSystolicBP() < 0) {
-      errorsExist = true;
-      errorMessage.append("-Invalid Systolic Blood Pressure<br>\n");
-    }
-    //dia
-    if (form.getDiastolicBP() < 0) {
-      errorsExist = true;
-      errorMessage.append("-Invalid Diastolic Blood Pressure<br>\n");
-    }
-
-    //test weight
-    if (form.getPatientWeight() < 0) {
-      errorsExist = true;
-      errorMessage.append("-Invalid Patient Height<br>\n");
-    }
-
-    //test height
-    if (form.getPatientHeight() < 0) {
-      errorsExist = true;
-      errorMessage.append("-Invalid Patient Weight<br>\n");
-    }
-
-    //test more specific things
-
-    //if pos sickle cell ensure type is selected
-    if (form.getSicklingStatus().equals("Positive") && form.getSicklingType().equals("")) {
-      errorsExist = true;
-      errorMessage.append("-Please Add Sickling Type, or select N/A");
-    }
-
-    //TODO: Add more error messages
-    //if error found produce pop up
-    if (errorsExist) {
-      //TODO: Remove once everything works okay
-      form.errorMessage(errorMessage.toString());
-      return false;
-    }
-    return true;
   }
 
   public JPanel getPanel() {
